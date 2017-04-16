12687fee314c8d1775a288bdae60897697d039ac
= <=>
2017-04-16 11:55:45 -0400

    add charts

--- simulate.py
+++ simulate.py
@@ -1,5 +1,8 @@
 
 import random
+import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -8,9 +11,30 @@ def main():
         r = roll()
         for x in r:
             results[x] += 1
+    results_dec = map(lambda x: math.floor(x*1.0/(i/100))/100.0 , results)
+    print results_dec
+    
+    plt =  make_chart([ (x,y) for x,y in enumerate(results_dec) if x >= 2])
+    plt.savefig('machi_koro_rolls.png',bbox_inches= 'tight')
+
+
+def make_chart(results):
     print results
+    data = results
+    data.sort(key = lambda x: x[0])
+    print data
+    X = map(lambda x: x[0] , data)
+    y = map(lambda x: x[1] , data)
+    y_pos = np.arange(len(X))
 
+    plt.bar(y_pos,y,align = 'center' , alpha = 0.5)
+    plt.xticks(y_pos, X)
+    plt.ylabel('Probability of being able to make roll')
+    plt.title('Machi Koro endgame roll distribution')
 
+#    plt.show()
+
+    return plt
 
 def roll():
     d1 = random.randint(1,6)
